84d184a56175347d8acf4848b21816c731187e36
Adrian Leonardo <48730762+toollsdev@users.noreply.github.com>
2025-03-20 13:38:45 -0300

    fix errors

--- app/src/main/java/com/example/maracylavanderia/MainActivity.kt
+++ app/src/main/java/com/example/maracylavanderia/MainActivity.kt
@@ -23,7 +23,7 @@ import java.io.File
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    private val currentVersion = BuildConfig.VERSION_NAME // Obtém a versão dinamicamente
+    private val currentVersion = "1.0.0" // Obtém a versão dinamicamente
     private lateinit var apkFile: File
 
     private lateinit var webview: WebView
